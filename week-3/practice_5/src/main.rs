ac23ddc503abd539b74e96628beb910047af523a
Arinze Nwafor <arinze.nwafor.pau.edu.ng>
2022-10-28 17:47:04 +0100

    (Week-3 Practices on Rust with cargo)

---
+++ week-3/practice_5/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let special_character = '@'; //default
+    let alphabet:char = 'N';
+    let surname = " Nwafor ";
+    let first_name = " Arinze ";
+    let middle_name = " Benjamin ";
+
+    println!( );
+    println!(" special character: {}", special_character);
+    println!("  Alphabet: {} ", alphabet);
+    println!(" Name: {} {} {} ", surname,middle_name,first_name);
+}
